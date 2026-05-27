585b4df7ce819ffb4e70550a99c92d8b9d1bf28b
Oguz Ozsahin <oguzozshn@gmail.com>
2026-05-27 18:00:23 +0300

    refactor(stack): replace ArrayList with array implementation

--- src/DataStructures/Stack.java
+++ src/DataStructures/Stack.java
@@ -1,32 +1,50 @@
 package DataStructures;
 
-import java.util.ArrayList;
 import Model.Patient;
 
 public class Stack {
-    private ArrayList<Patient> stack;
+    private Patient[] stack;
     private int top;
+    private int capacity;
 
-    public Stack() {
-        stack = new ArrayList<>();
-        top = -1;
-    };
+    public Stack(int capacity) {
+        this.capacity = capacity;
+        this.stack = new Patient[capacity];
+        this.top = -1;
+    }
 
-    public void push(Patient patient){
-        stack.add(top, patient);
+    public void push(Patient patient) {
+        if (isFull()) {
+            return;
+        }
         top++;
-    };
+        stack[top] = patient;
+    }
 
-    public void pop(){
-        stack.remove(top);
+    public Patient pop() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        Patient tempPatient = stack[top];
+        stack[top] = null;
         top--;
-    };
 
-    public Patient peek(){
-        return stack.get(top);
-    };
+        return tempPatient;
+    }
 
-    public boolean isEmpty(){
+    public Patient peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return stack[top];
+    }
+
+    public boolean isEmpty() {
         return top == -1;
-    };
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
 }

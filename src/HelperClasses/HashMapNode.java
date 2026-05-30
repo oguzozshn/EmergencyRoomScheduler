d193d81a340bdd731a6a454a95c3ff8064008b38
Oguz Ozsahin <oguzozshn@gmail.com>
2026-05-30 09:19:31 +0300

    refactor(HashMapNode): Implemented logic

--- src/HelperClasses/HashMapNode.java
+++ src/HelperClasses/HashMapNode.java
@@ -1,5 +1,13 @@
 package HelperClasses;
 
-public class HashMapNode {
+import DataStructures.HashMap;
 
+public class HashMapNode {
+    public Object key;
+    public Object value;
+
+    public HashMapNode(Object key, Object value){
+        this.key = key;
+        this.value = value;
+    }
 }
